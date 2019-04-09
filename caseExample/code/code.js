7bfa396827492f0fd2cd5dec9b772df770b456d6
Hyhello <673089899@qq.com>
2019-04-09 17:34:26 +0800

    feat: code

--- caseExample/code/code.js
+++ caseExample/code/code.js
@@ -1,117 +1,218 @@
-(function (global) {
-    // 验证码
-    function Code (el, options = {}) {
-        this.$ctx = el.getContext('2d');
-        // 校验
-        this.constructor.checkConf(options);
-        this.$opt = this.constructor.extend(true, this.constructor.CONF, options);
-        this.$dpr = this.constructor.getPixelRatio(this.$ctx);
-        el.width = this.$opt.width *= this.$dpr;
-        el.height = this.$opt.height *= this.$dpr;
-        console.log(this.$opt);
-        this._refresh();
-    };
+/**
+ * 作者：yeshengqiang
+ * 时间：2019-04-04
+ * 描述：验证码
+ */
 
-    Code.getPixelRatio = function (context) {
-        var backingStore = context.backingStorePixelRatio ||
-            context.webkitBackingStorePixelRatio ||
-            context.mozBackingStorePixelRatio ||
-            context.msBackingStorePixelRatio ||
-            context.oBackingStorePixelRatio ||
-            context.backingStorePixelRatio || 1;
-        return (window.devicePixelRatio || 1) / backingStore;
-    };
+"use strict";
 
-    // 配置
-    Code.CONF = {
-        width: 300,                                             // 画布宽度
-        height: 150,                                            // 画布高度
-        clipWidth: 45,                                          // 剪切宽度
-        clipHeight: 45	                                        // 剪切高度
-        // imgSrcList: ['./img/code1.jpg', './img/', '', '', '']
-    };
+function _instanceof(left, right) { if (right != null && typeof Symbol !== "undefined" && right[Symbol.hasInstance]) { return right[Symbol.hasInstance](left); } else { return left instanceof right; } }
 
-    Code.checkConf = function (options) {
-        var CONF = this.CONF;
-        Object.keys(CONF).forEach(function (item, key) {
-            var type = typeof CONF[item];
-            if (options[item] && typeof options[item] !== type) {
-                throw new TypeError('[Code] options property：' + item + ' is not a ' + type);
-            }
-        });
-    };
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!_instanceof(instance, Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+var Code =
+/*#__PURE__*/
+function () {
+  function Code(id, options, callback) {
+    _classCallCheck(this, Code);
+
+    var $dpr = window.devicePixelRatio || 1;
+    this.$el = Code.isHTMLElement(id) ? id : document.getElementById(id);
+    var rect = this.$el.getBoundingClientRect();
+
+    if (!rect.height) {
+      console.warn('parent box must has height');
+      return;
+    }
+
+    options = options || {}; // 校验
+
+    Code._checkConf(options);
+
+    this.callback = callback || function () {};
+
+    this._canvas = document.createElement('canvas');
+    this._canvas.innerHTML = '您的浏览器版本不支持canvas';
+    this.$el.appendChild(this._canvas);
+    this.$ctx = this._canvas.getContext('2d'); // 配置
+
+    this.$conf = Code._extend({}, Code.defaults, options);
+    this.$conf.width = rect.width;
+    this.$conf.height = rect.height; // 配置宽高
+
+    this._canvas.width = rect.width * $dpr;
+    this._canvas.height = rect.height * $dpr;
+    this._canvas.style.width = this.$conf.width + 'px';
+    this._canvas.style.height = this.$conf.height + 'px';
+    this.$ctx.scale($dpr, $dpr); // 开始draw
+
+    this._refresh(); // fire
 
 
-    Code.extend = function () {
-        var target = arguments[0];
-        var argList = Array.prototype.slice.call(arguments, 1);
-        var argLen = argList.length;
-        target = typeof target === 'boolean' ? {} : target;
-        while (argLen--) {
-            var resource = argList[argLen];
-            for (var i in resource) {
-                if (resource.hasOwnProperty(i)) {
-                    target[i] = resource[i];
-                }
-            }
+    this._fire();
+  }
+
+  _createClass(Code, [{
+    key: "_refresh",
+    value: function _refresh() {
+      this.$ctx.clearRect(0, 0, this.$conf.width, this.$conf.height);
+
+      var codeStr = this._drawText();
+
+      this._drawLine();
+
+      this._drawDot();
+
+      this.callback(codeStr);
+    }
+  }, {
+    key: "_drawText",
+    value: function _drawText() {
+      var len = this.$conf.codeLen;
+      var partWidth = this.$conf.width / (len + 1);
+      var codeStr = '';
+
+      for (var i = 0; i < len; i++) {
+        this.$ctx.beginPath();
+        this.$ctx.save();
+
+        var rotate = Code._randomNum(-45, 45);
+
+        var fontSize = Code._randomNum(this.$conf.height * 0.5, this.$conf.height * 0.8);
+
+        var fillText = this.$conf.randomStr.charAt(Code._randomNum(0, this.$conf.randomStr.length));
+        codeStr += fillText;
+        this.$ctx.translate((i + 1) * partWidth, Code._randomNum(fontSize / 2, this.$conf.height - fontSize / 2 - 5));
+        this.$ctx.rotate(rotate * Math.PI / 180);
+        this.$ctx.font = '600 ' + fontSize + 'px Georgia';
+        this.$ctx.textAlign = 'center';
+        this.$ctx.textBaseline = 'middle';
+        this.$ctx.fillStyle = Code._randomColor(Code._randomNum(0, 360));
+        this.$ctx.fillText(fillText, 0, 0);
+        this.$ctx.restore();
+      }
+
+      return codeStr;
+    }
+  }, {
+    key: "_drawLine",
+    value: function _drawLine() {
+      this.$ctx.save();
+      this.$ctx.lineWidth = 1;
+      this.$ctx.globalAlpha = 0.5;
+
+      for (var i = 0; i < this.$conf.lineNum; i++) {
+        this.$ctx.beginPath();
+        this.$ctx.strokeStyle = Code._randomColor(Code._randomNum(100, 300));
+        this.$ctx.moveTo(Code._randomNum(0, this.$conf.width), Code._randomNum(0, this.$conf.height));
+        this.$ctx.lineTo(Code._randomNum(0, this.$conf.width), Code._randomNum(0, this.$conf.height));
+        this.$ctx.stroke();
+      }
+
+      this.$ctx.restore();
+    }
+  }, {
+    key: "_drawDot",
+    value: function _drawDot() {
+      for (var i = 0; i < this.$conf.dotNum; i++) {
+        this.$ctx.beginPath();
+        this.$ctx.save();
+        this.$ctx.translate(Code._randomNum(0, this.$conf.width), Code._randomNum(0, this.$conf.height));
+        this.$ctx.globalAlpha = 0.5;
+        this.$ctx.fillStyle = Code._randomColor(Code._randomNum(100, 300));
+        this.$ctx.arc(0, 0, 1, Math.PI * 2, false);
+        this.$ctx.fill();
+        this.$ctx.restore();
+      }
+    }
+  }, {
+    key: "_fire",
+    // 事件
+    value: function _fire() {
+      var _this = this;
+
+      this.$el.__outSideClick__ = function () {
+        _this._refresh();
+      };
+
+      this.$el.addEventListener('ontouchstart' in document.documentElement ? 'touchstart' : 'click', this.$el.__outSideClick__, false);
+    }
+  }, {
+    key: "_destroy",
+    value: function _destroy() {
+      this.$el.removeEventListener('ontouchstart' in document.documentElement ? 'touchstart' : 'click', this.$el.__outSideClick__, false);
+      this.$ctx.clearRect(0, 0, this.$conf.width, this.$conf.height);
+      this.$el.removeChild(this._canvas);
+      this.$el.__outSideClick__ = this._canvas = null;
+    }
+  }], [{
+    key: "isHTMLElement",
+    value: function isHTMLElement(el) {
+      return _typeof(el) === 'object' && el.nodeType === 1;
+    }
+  }, {
+    key: "_checkConf",
+    value: function _checkConf(options) {
+      var CONF = this.defaults;
+      Object.keys(CONF).forEach(function (item, key) {
+        var type = _typeof(CONF[item]); // eslint-disable-next-line valid-typeof
+
+
+        if (options[item] && _typeof(options[item]) !== type) {
+          throw new TypeError('[Code] options property：' + item + ' is not a ' + type);
         }
-        return target;
-    };
+      });
+    }
+  }, {
+    key: "_extend",
+    value: function _extend(target) {
+      var i, len, args, resource;
+      if (Object.assign) return Object.assign.apply(Object, arguments);
+      target = typeof target === 'boolean' ? {} : target;
+      args = Array.prototype.slice.call(arguments, 1);
+      len = args.length;
 
-    Code.loadImg = function (src, callback) {
-        var img = new Image();
-        img.onload = function () {
-            callback(img);
-        };
-        img.onerror = function () {
-            throw new Error('[Code] load image fail');
-        };
-        img.src = src;
-    };
+      while (len--) {
+        resource = args[len];
 
-    Code.prototype = {
-        // 版本
-        versoin: '0.0.1',
-
-        constructor: Code,
-
-        // 刷新
-        _refresh () {
-            var _this = this;
-            this.$ctx.clearRect(0, 0, this.$ctx.canvas.width, this.$ctx.canvas.height);
-            this.constructor.loadImg('./img/code3.jpg', function (data) {
-                _this._init(data);
-            });
-        },
-
-        // 初始化
-        _init: function (data) {
-            this.$ctx.drawImage(data, 0, 0);
-            this._clipShell();
-        },
-
-        // 剪切壳子
-        _clipShell () {
-            this.$ctx.save();
-            this.$ctx.translate(this.$ctx.canvas.width / 2, this.$ctx.canvas.height / 2);
-
-            // 正方形
-            this.$ctx.beginPath();
-            // this.$ctx.fillStyle = 'rgba(255, 255, 255, 0.6)';
-            this.$ctx.strokeStyle = '#FFFFFF';
-            this.$ctx.rect(-this.$opt.clipWidth / 2, -this.$opt.clipHeight / 2, this.$opt.clipWidth, this.$opt.clipHeight);
-            this.$ctx.fill();
-            this.$ctx.stroke();
-
-            this.$ctx.beginPath();
-            this.$ctx.globalCompositeOperation = 'destination-out';
-            this.$ctx.arc(0, 15, this.$opt.clipWidth / 5, 0, Math.PI * 2, false);
-
-            this.$ctx.fill();
-
-            this.$ctx.restore();
+        for (i in resource) {
+          target[i] = resource[i];
         }
-    };
+      }
 
-    global.Code = Code;
-} ( window ));
+      return target;
+    }
+  }, {
+    key: "_randomColor",
+    // 随机颜色 r 取值范围 0 ~ 360度
+    value: function _randomColor(r) {
+      r = r * Math.PI / 180;
+      var pi = Math.PI * 2;
+      var cos = Math.cos;
+      return '#' + (cos(r) * 127 + 128 << 16 | cos(r + pi / 3) * 127 + 128 << 8 | cos(r + pi / 3 * 2) * 127 + 128).toString(16);
+    }
+  }, {
+    key: "_randomNum",
+    value: function _randomNum(min, max) {
+      return Math.floor(Math.random() * (max - min) + min);
+    }
+  }]);
+
+  return Code;
+}();
+
+_defineProperty(Code, "defaults", {
+  codeLen: 4,
+  lineNum: 5,
+  dotNum: 50,
+  dotText: '.',
+  randomStr: 'ABCDEFGHIJKLMOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+});
